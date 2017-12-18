2fae38443f1048af3f485be0bf6e9eb3f747c46d
song.zhang <zhangs@inke.cn>
2017-12-18 20:34:43 +0800

    第二次代码提交

--- src/test/java/hdfs/UserAgentTest.java
+++ src/test/java/hdfs/UserAgentTest.java
@@ -38,7 +38,7 @@ public class UserAgentTest {
      */
     @Test
     public void userAgentTest() {
-        String source = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MDDR; InfoPath.2; .NET4.0C)";
+        String source = "Yammer 4.1.1.668 (iPhone; iPhone OS 5.0.1; en_US)";
 
         UserAgentParser userAgentParser = new UserAgentParser();
         UserAgent agent = userAgentParser.parse(source);
